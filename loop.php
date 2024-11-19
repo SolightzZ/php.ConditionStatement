1b035a70ba4eb60751635bc753b75b7b57368ec6
SolightzZ <reimdes1@gmail.com>
2024-11-19 11:16:12 +0700

    Update loop.php

--- loop.php
+++ loop.php
@@ -6,19 +6,60 @@
     <title>Document</title>
 </head>
 <body>
-    <h1> การใช้คำสั่งแบบว loop Stringable </h1>
+    <h1> การใช้คำสั่งแบบ loop Stringable </h1>
     <?php
         echo "<h2> Whli loop </h2>";
         echo "<br>";
 
         $x = 1;
+        $y = 1;
+
         while ($x <= 5) {
-            echo "วนรอบที่ : $x <br>";
+            //if ( $x == 3) break ; 
+            echo " Whli วนรอบที่ : $x <br>";
             $x++;
+
         }
         echo "<br>";
-        $l = 1;
-        while ($l <= 80 )   {echo "-"; $l++;}
+        $i = 1;
+        while ($i <= 80 )   {echo "-"; $i++;}
+        echo "<br>";
+
+        echo "<h2> Do Whli loop </h2>";
+        echo "<br>";
+        do {
+            echo " Do Whli วนรอบที่ : $y <br>";
+            $y++;
+            //if ($y <= 5) break; 
+        } while ($y <= 5);
+        echo "<br>";
+        $i = 1;
+        while ($i <= 80 )   {echo "-"; $i++;}
+        echo "<br>";
+
+        echo "<h2> For loop </h2>";
+        echo "<br>";
+        for ($i=1; $i<=10; $i++) {
+            echo "For รอบที่ $i <br>";
+        }
+        echo "<br>";
+        $i = 1;
+        while ($i <= 80 )   {echo "-"; $i++;}
+        echo "<br>";
+
+
+        echo "<h2> * For ซ่อนกัน</h2>";
+        echo "<br>";
+        for ($i=1; $i<=5; $i++) {
+            for ($j= 1; $j<=10; $j++) {
+                echo "*";
+            }
+            echo "<br>";
+        } 
+        
+        echo "<br>";
+        $i = 1;
+        while ($i <= 80 )   {echo "-"; $i++;}
         echo "<br>";
     ?>
 </body>
